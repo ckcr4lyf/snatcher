1f8c807e7b6c3440f72c62a108c9bf2bb25a266d
Raghu Saxena <poiasdpoiasd@live.com>
2024-07-02 18:28:20 +0800

    Regex matching

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,8 +1,10 @@
 use log::debug;
+use regex;
 
 use crate::trackers;
 
 pub struct Filter {
+    valid_regexes: regex::RegexSet,
     size_max: i64,
 }
 
@@ -13,6 +15,11 @@ impl Filter {
             return false;
         }
 
+        if self.valid_regexes.matches(torrent.name()).matched_any() == false {
+            debug!("Torrent name {} did not match any of the regexes!", torrent.name());
+            return false;
+        }
+
         debug!("All checks pass!");
         return true;
     }
@@ -20,11 +27,14 @@ impl Filter {
 
 #[cfg(test)]
 mod tests {
+    use regex::RegexSet;
+
     use super::*;
 
     #[derive(Debug)]
     struct DummyTorrent {
         size: i64,
+        name: String,
     }
 
     impl trackers::Torrent for DummyTorrent {
@@ -33,7 +43,7 @@ mod tests {
         }
 
         fn name(&self) -> &str {
-            unimplemented!()
+            return &self.name;
         }
 
         fn path(&self) -> &std::ffi::OsStr {
@@ -45,12 +55,20 @@ mod tests {
     fn check(){
         let filter = Filter{
             size_max: 4000,
+            valid_regexes: RegexSet::new(&[r"^YOLO$"]).unwrap(),
         };
         
-        let dummy = DummyTorrent{
-            size: 100
+        let dummy_valid = DummyTorrent{
+            size: 100,
+            name: "YOLO".to_owned(),
         };
 
-        assert_eq!(filter.check(dummy), true);
+        let dummy_invalid = DummyTorrent{
+            size: 100,
+            name: "XD".to_owned(),
+        };
+
+        assert_eq!(filter.check(dummy_valid), true);
+        assert_eq!(filter.check(dummy_invalid), false);
     }
 }
