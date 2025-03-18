2ddc0ce637975696de31d9db229c1a20216747bf
Raghu Saxena <poiasdpoiasd@live.com>
2025-03-18 14:01:13 +0800

    Remove unused function

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,32 +1,7 @@
 // TODO: maybe trait?
 
 use std::ffi::OsStr;
-
 use log::{debug, error, info, trace};
-
-use crate::trackers;
-
-pub fn add_to_qbit(torrent: impl trackers::Torrent) {
-    trace!("Going to add {}", torrent.name());
-
-    // Assume qbit-race.
-    // mix args and arg so we can pass str and OsStr
-    match std::process::Command::new("qbit-race")
-        .args(["add", "-p"])
-        .arg(torrent.path())
-        .output()
-    {
-        Ok(output) => {
-            debug!(
-                "Added to qbittorrent: {}",
-                String::from_utf8_lossy(&output.stdout)
-            );
-        }
-        Err(e) => {
-            error!("Failed to add: {}", e);
-        }
-    }
-}
 
 pub fn add_to_qbit_v2(path: &OsStr) {
     trace!("Going to add {:?}", path);
